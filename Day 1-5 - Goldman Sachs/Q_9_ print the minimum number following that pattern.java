ba294cafa234dc497d8ea2fe4203ebe090741918
Nandan26 <77192056+Nandan26@users.noreply.github.com>
2022-01-02 11:48:50 +0530

    Create Q_9_ print the minimum number following that pattern.java

---
+++ Day 1-5 - Goldman Sachs/Q_9_ print the minimum number following that pattern.java
@@ -0,0 +1,25 @@
+class Solution{
+    static String printMinNumberForPattern(String S){
+        String ans = "";
+
+        Stack<Integer> st = new Stack<Integer>();
+ 
+        for (int i = 0; i <S.length(); i++) {
+
+            st.push(i + 1);
+
+            if (S.charAt(i) == 'I') {
+                
+                while (!st.empty()) {
+
+                    ans += String.valueOf(st.pop());
+                }
+            }
+        }
+        st.push(S.length()+1);
+        while (!st.empty()) {
+            ans += String.valueOf(st.pop());
+        }
+        return ans;
+    }
+}
