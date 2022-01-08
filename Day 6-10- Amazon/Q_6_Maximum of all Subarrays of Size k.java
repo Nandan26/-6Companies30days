c0809e2fbecacfbad6378210948fd75c7f48d8f2
Nandan26 <77192056+Nandan26@users.noreply.github.com>
2022-01-08 12:53:15 +0530

    Create Q_6_Maximum of all Subarrays of Size k.java

---
+++ Day 6-10- Amazon/Q_6_Maximum of all Subarrays of Size k.java
@@ -0,0 +1,34 @@
+//Similar to Q-3
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        ArrayList<Integer> res=new ArrayList<>();
+        Deque<Integer> dq=new LinkedList<>();
+        int i=0,j=0;
+        int t=0;
+        while(j<n)
+        {
+            
+            while(!dq.isEmpty() && arr[dq.peekLast()]<arr[j])
+            {
+                dq.pollLast();
+            }
+            dq.offerLast(j);
+            
+            
+            if(j-i+1==k)
+            {
+                
+                res.add(arr[dq.peekFirst()]);
+                
+                if(dq.peekFirst()==i)
+                    dq.pollFirst();
+                i++;
+            }
+            
+            if((j-i+1)<k)
+                j++;
+        }
+        return res;
+        
+    }
+}
