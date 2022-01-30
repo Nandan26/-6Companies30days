e4c790004102503b1364abe79e16e4ce803950e9
Nandan26 <77192056+Nandan26@users.noreply.github.com>
2022-01-30 12:36:30 +0530

    Add files via upload

---
+++ Day 26-30_walmart/Q_5_Sum tree.java
@@ -0,0 +1,16 @@
+class Solution{
+
+    public void toSumTree(Node root){
+        helper(root);
+    }
+    private int helper(Node root){
+        if(root==null) return 0;
+
+        int leftSum= helper(root.left);
+        int rightSum=helper(root.right);
+
+        int prev=root.data;
+        root.data=leftSum+rightSum;
+        return prev+root.data;
+    }
+}
